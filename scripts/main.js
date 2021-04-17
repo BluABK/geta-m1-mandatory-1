76a3347c5a2bdb9c56464e3e323a0d3665eb77b8
BluABK <bluabk@bluabk.net>
2021-04-17 02:15:09 +0200

    Rewrote card piles to use Objects not Arrays of HTML Strings.
    - Created Card object.
    - Created Cards object to hold Card objects. The Cards object mainly consists of an items array and an items Map (points to Card objects by UUID). It also implements the following Array-like behaviour:
        - Iterator that iterates its items Array.
        - Length getter that returns its items Array's length.
        - Includes function that proxies its items Array's include function.
        - Shift function that pops the first item of its items Array, and then deletes the mapped entry.
        - Push function that appends a given item to its items Array, and then sets a mapped entry.
        - Pop function that pops the last item of its items Array, and then deletes the mapped entry.
        - Shuffle function (ported from global scope) that performs an unbiased Durstenfield shuffle on its items Array.
    - Shuffle function was moved into Cards and ported accordingly.
    - More card name terminology renaming.
    - Created getCardHTML function that takes a Card object and returns the HTML String to display on the page.
    - Ported createDeck to the new Object-based system.
    - Created getCardOwnerIndex function that determines and then returns the playersDeck index of the player that currently holds the given card.
    - Created moveCard function that moves a card from one pile to another.
    - Ported dealCards to the new Object-based system.

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,179 @@
 // Model:
 // As the value of Ace depends upon the card game, define it as a constant that can easily be changed later.
 const ACE_FACE_CARD_VALUE = 14;
-const CARD_FAMILIES = ["heart", "diamond", "spade", "club"];
+const SUITS = ["heart", "diamond", "spade", "club"];
 // Card that cannot be used in play (usually happens with odd amount of players with an even amount of source deck cards).
 let deadCard = null;
-// Cards players draw from.
-let playerDecks = [[], []];
-// Cards players have played.
-let playerPiles = [[], []];
-const CARD_FACING_BACK = true;
-const CARD_FACING_FRONT = false;
-const CARD_DECK = createDeck(CARD_FACING_BACK);
-// Make card deck immutable to avoid modifications, as const only protects it from re-assignment.
-Object.freeze(CARD_DECK);
 
+/**
+ * Generate UUID v4 string to use for IDs and similiar.
+ * @returns {String} An UUID.
+ */
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+    });
+}
+
+// Classes.
+class Card {
+    constructor(value, suit, owner = null) {
+        this.id = uuidv4();
+        this.value = value;
+        this.suit = suit;
+        this.ownerId = owner;
+    }
+
+    set owner(playerId) {
+        this.ownerId = playerId;
+    }
+
+    get owner() {
+        return this.owner;
+    }
+}
+
+class Cards {
+    items = [];
+    itemsMap = null;
+
+    constructor(items = []) {
+        if (items instanceof Array) {
+            this.items = items;
+            this.itemsMap = new Map();
+
+            for (let item of items) {
+                // Append a reference to the card by its UUID to the items map.
+                this.itemsMap.set(item.id, item);
+            }
+        } else {
+            throw new TypeError("Constructor argument must be instance of Array!");
+        }
+    }
+
+    /**
+     * Make object iterable by implementing a specific next() method.
+     * @returns Iterator
+     */
+    [Symbol.iterator]() {
+        let index = -1;
+
+        return {
+            // Iterate through items array until end boundary has been reached.
+            next: () => ({ value: this.items[++index], done: !(index in this.items) })
+        };
+    };
+
+    /**
+     * Pass .length to items array.
+     */
+    get length() {
+        return this.items.length;
+    }
+
+    /**
+     * Pass .include to items array.
+     * @param {any} searchElement
+     * @param {number} fromIndex
+     * @returns boolean
+     */
+    includes(searchElement, fromIndex = undefined) {
+        return this.items.includes(searchElement, fromIndex);
+    }
+
+    shift() {
+        // Pop item from the start of items array.
+        let poppedItem = this.items.shift();
+
+        // Delete popped item from the items map.
+        this.itemsMap.delete(poppedItem.id);
+
+        return poppedItem;
+    }
+
+    push(card) {
+        // Append card to items array.
+        this.items.push(card);
+
+        // Append a reference to the card by its UUID to the items map.
+        this.itemsMap.set(card.id, card);
+    }
+
+    pop() {
+        // Pop item from the end of items array.
+        let poppedItem = this.items.pop();
+        // Delete popped item from the items map.
+        this.itemsMap.delete(poppedItem.id);
+
+        return poppedItem;
+    }
+
+    /**
+     * Shuffle a deck in-place using the unbiased Durstenfield shuffle algorithm.
+     * Time complexity: O(n).
+     *
+     * It picks a random element for each original array element, and excludes it from the next draw,
+     * like picking randomly from a deck of cards.
+     *
+     * This clever exclusion swaps the picked element with the current one, then picks the next random
+     * element from the remainder, looping backwards for optimal efficiency, ensuring the random pick is
+     * simplified (it can always start at 0), and thereby skipping the final element.
+     *
+     * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+     * @returns Success status (boolean).
+     */
+    shuffle() {
+        if (this.items instanceof Array == false) {
+            return false;
+        }
+
+        if (this.items.length == 0) {
+            return false;
+        }
+
+        for (let i = this.items.length - 1; i > 0; i--) {
+            // Assign j a random integer such that 0 <= j <= i.
+            const j = Math.floor(Math.random() * (i + 1));
+
+            // Exchange deck[j] and deck[i]:
+            // Avoid spending needless time counting the remaining numbers by moving the "struck" numbers
+            // to the end of the list by swapping them with the last "unstruck" number at each iteration.
+            [ this.items[i], this.items[j] ] = [ this.items[j], this.items[i] ];
+        }
+
+        return true;
+    }
+}
+
+// Cards players draw from.
+let playerDecks = [new Cards(), new Cards()];
+// Cards players have played.
+let playerPiles = [new Cards(), new Cards()];
+
+const CARD_FACING_BACK = true;
 
 // View:
 function updateView() {
+    /**
+     * Notes:
+     * Each player deck and discard pile only displays the last card from their respective stack.
+     */
     document.getElementById("app").innerHTML = `
         <div class="board">
             <div class="board-top-part">
-                <div id="player1-deck" class="card-slot">
-                    ${playerDecks[0][0]}
+                <div id="player1-deck" class="card-slot card-deck" playersIndex="0">
+                    ${playerDecks[0][0] ? getCardHTML(playerDecks[0][0], CARD_FACING_BACK) : ""}
                 </div>
-                <div id="player1-display" class="card-slot"></div>
+                <div id="player1-pile" class="card-slot card-pile" playersIndex="0">
+                    ${playerPiles[0][0] ? playerPiles[0][0] : ""}
+                </div>
                 <div class="card-gap"></div>
-                <div id="player2-display" class="card-slot"></div>
-                <div id="player2-deck" class="card-slot">
-                    ${playerDecks[1][0]}
+                <div id="player2-pile" class="card-slot card-pile" playersIndex="1">
+                    ${playerPiles[1][0] ? playerPiles[1][0] : ""}
+                </div>
+                <div id="player2-deck" class="card-slot card-deck" playersIndex="1">
+                    ${playerDecks[1][0] ? playerDecks[1][0] : ""}
                 </div>
             </div>
         </div>
@@ -35,27 +181,28 @@ function updateView() {
 }
 
 // Controller:
+
 /**
  * Create a nice card front design.
  * @param {*} value The actual value (number) of the card.
- * @param {*} family Card family.
+ * @param {*} suit Card suit.
  * @param {*} hide Whether or not to display the design.
  * @returns 
  */
-function createCardFrontDesignHTML(value, family, hide = true) {
+function createCardFrontDesignHTML(value, suit, hide = true) {
     let symbol = "";
 
-    if (family === "heart") {
+    if (suit === "heart") {
         symbol = "♥";
-    } else if (family === "diamond") {
+    } else if (suit === "diamond") {
         symbol = "♦";
-    } else if (family === "spade") {
+    } else if (suit === "spade") {
         symbol = "♠";
-    } else if (family === "club") {
+    } else if (suit === "club") {
         symbol = "♣";
     } else {
-        console.error("createCardDesignHTML got unknown card family!", family);
-        symbol = family;
+        console.error("createCardDesignHTML got unknown card suit!", suit);
+        symbol = suit;
     }
 
     // Handle face card values.
@@ -75,23 +222,33 @@ function createCardFrontDesignHTML(value, family, hide = true) {
     return myDiv;
 }
 
+function getCardHTML(cardObject, facingBack = false) {
+    let cardDesign = createCardFrontDesignHTML(cardObject.value, cardObject.suit, facingBack);
+    let cardHTML = `<div class="card ${cardObject.suit} card-facing-${facingBack ? "back" : "front"} clickable" value="${cardObject.value}" onClick="clickedCard(this)" cardId="${cardObject.id}">${cardDesign}</div>`
+
+    return cardHTML;
+}
+
 /**
- * Create a deck of cards.
+ * Create a deck of Card objects.
  * @param {*} facingBack Card faces down.
- * @returns 
+ * @returns Array of initialised Card objects.
  */
-function createDeck(facingBack = false) {
-    let deck = [];
-    // For each card family:
-    for (let fam of CARD_FAMILIES) {
+function createDeck() {
+    let deck = new Cards();
+    // For each card suit:
+    for (let suit of SUITS) {
         // For each card value:
         for (let i = 2; i <= 14; i++) {
-            let cardDesign = createCardFrontDesignHTML(i, fam, facingBack);
-            deck.push(`<div class="card ${fam} card-facing-${facingBack ? "back" : "front"} clickable" value="${i}" onClick="clickedCard(this)">${cardDesign}</div>`)
+            deck.push(new Card(i, suit))
         }
     }
 
     return deck;
+}
+
+function getCardOwnerIndex(card) {
+    return parseInt(card.parentElement.getAttribute("playersIndex"));
 }
 
 function cardFaceFront(card) {
@@ -117,23 +274,48 @@ function flipCard(card) {
     }
 }
 
+/**
+ * Moves a card from one pile to another.
+ * @param {HTMLElement} card Card.
+ * @param {Array} src Source pile.
+ * @param {Array} dst Destination pile.
+ */
+function moveCard(card, source, destination) {
+    // Get card's position in source array.
+    let cardSourceIndex = source.indexOf(card);
+    console.log(cardSourceIndex)
+
+    if (cardSourceIndex == -1) {
+        console.error("Attempted to move a card from a source it isn't in.", card, source);
+        return;
+    }
+
+    // Remove card from its source array.
+    let removedCard = source.splice(cardSourceIndex, 1);
+    // Append card to the destination array.
+    destination.push(card);
+
+    return removedCard;
+}
+
 function clickedCard(card) {
+    console.log("clickedCard(card)", card);
     if (card.classList.contains("clickable")) {
+        let ownerIndex = getCardOwnerIndex(card);
+
+        moveCard(card, playerDecks[ownerIndex], playerPiles[ownerIndex]);
         flipCard(card);
     }
 }
 
-function dealCards(playerDecks, cardDeck) {
+function dealCards(playerDecks, cardsObject) {
     if (playerDecks.length == 0) {
         console.error("Attempted to deal cards to no players!");
         return;
     }
 
-    // Copy the deck of cards into cards variable.
-    let cards = Object.assign([], cardDeck);
-
     // Shuffle the deck of cards.
-    if (!shuffleDeck(cards)) {
+    if (!cardsObject.shuffle()) {
         console.error("Failed to shuffle cards, expect breakage!");
         alert("Failed to shuffle cards, expect breakage!");
     }
@@ -141,26 +323,26 @@ function dealCards(playerDecks, cardDeck) {
     if (playerDecks.length % 2 != 0) {
         // If there are an odd number of players
 
-        // Shuffle the deck in case it wasn't already shuffled.
-        shuffleDeck(cards);
+        // // Shuffle the deck in case it wasn't already shuffled.
+        // shuffleDeck(cardsObject);
 
         // Discard a (now definitely) random card, by popping the first element.
-        deadCard = cards.shift();
+        deadCard = cardsObject.shift();
 
         console.info(`Discarded card due to odd number of players (${playerDecks.length})`, deadCard);
 
         return;
     }
 
-    let cardsPerPlayer = cards.length / playerDecks.length;
+    let cardsPerPlayer = cardsObject.length / playerDecks.length;
 
     for (let i = 0; i < playerDecks.length; i++) {
         // Empty player's old deck, if any.
-        playerDecks[i] = [];
+        playerDecks[i] = new Cards();
 
         // Deal player their amount of cards from the given deck.
         for (let j = 0; j < cardsPerPlayer; j++) {
-            playerDecks[i].push(cards.pop());
+            playerDecks[i].push(cardsObject.pop());
         }
     }
 }
@@ -174,41 +356,6 @@ function getDeckHTML(playerDeck) {
     return deckHTML;
 }
 
-/**
- * Shuffle a deck in-place using the unbiased Durstenfield shuffle algorithm.
- * Time complexity: O(n).
- *
- * It picks a random element for each original array element, and excludes it from the next draw,
- * like picking randomly from a deck of cards.
- *
- * This clever exclusion swaps the picked element with the current one, then picks the next random
- * element from the remainder, looping backwards for optimal efficiency, ensuring the random pick is
- * simplified (it can always start at 0), and thereby skipping the final element.
- *
- * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
- * @param {*} deck A deck of cards.
- * @returns Success status (boolean).
- */
-function shuffleDeck(deck) {
-    if (deck instanceof Array == false) {
-        return false;
-    } else if (deck.length == 0) {
-        return false;
-    }
-
-    for (let i = deck.length - 1; i > 0; i--) {
-        // Assign j a random integer such that 0 <= j <= i.
-        const j = Math.floor(Math.random() * (i + 1));
-
-        // Exchange deck[j] and deck[i]:
-        // Avoid spending needless time counting the remaining numbers by moving the "struck" numbers
-        // to the end of the list by swapping them with the last "unstruck" number at each iteration.
-        [ deck[i], deck[j] ] = [ deck[j], deck[i] ];
-    }
-
-    return true;
-}
-
-dealCards(playerDecks, CARD_DECK);
+dealCards(playerDecks, createDeck());
 
 updateView();
--- scripts/tests.js
+++ scripts/tests.js
@@ -1,157 +1,163 @@
 function updateView() {} // Dummy function.
 
-QUnit.test("shuffleDeck - If supplied argument is null, return false", function (assert) {
-    let deck = null;
-    const succeeded = shuffleDeck(deck);
+QUnit.test("Cards - Cards constructor items argument must be instance of Array", function (assert) {
+    try {
+        new Cards(null);
+        assert.false(true, "Cards(null) was allowed!");
+    } catch(e) {
+        if (e instanceof TypeError) {
+            assert.true(true, "Cards(null) was not allowed.");
+        } else {
+            // Re-throw unexpected exception.
+            throw e;
+        }
+    }
+    try {
+        new Cards("A String");
+        assert.false(true, `Cards("A String") was allowed!`);
+    } catch(e) {
+        if (e instanceof TypeError) {
+            assert.true(true, `Cards("A String") was not allowed.`);
+        } else {
+            // Re-throw unexpected exception.
+            throw e;
+        }
+    }
+    try {
+        new Cards(Object);
+        assert.false(true, `Cards(Object) was allowed!`);
+    } catch(e) {
+        if (e instanceof TypeError) {
+            assert.true(true, `Cards(Object) was not allowed.`);
+        } else {
+            // Re-throw unexpected exception.
+            throw e;
+        }
+    }
+});
 
-    assert.strictEqual(deck, null, "Assert that deck is still null.");
+QUnit.test("Cards.shuffle - If Cards.items is empty, return false", function (assert) {
+    let cardsObject = new Cards([]);
+    let succeeded = cardsObject.shuffle();
+
+    assert.equal(cardsObject.items.length, 0, "Assert that deck has not been modified.");
     assert.equal(succeeded, false, "Assert that shuffle failed.");
 });
 
-QUnit.test("shuffleDeck - If supplied argument is undefined, return false", function (assert) {
-    let deck = undefined;
-    const succeeded = shuffleDeck(deck);
-
-    assert.strictEqual(deck, undefined, "Assert that deck is still undefined.");
-    assert.equal(succeeded, false, "Assert that shuffle failed.");
-});
-
-QUnit.test("shuffleDeck - If supplied argument is Object, return false", function (assert) {
-    let myObj = Object();
-    let deck = myObj;
-    const succeeded = shuffleDeck(deck);
-
-    assert.deepEqual(deck, myObj, "Assert that deck has not been modified.");
-    assert.equal(succeeded, false, "Assert that shuffle failed.");
-});
-
-QUnit.test("shuffleDeck - If supplied argument is [] (empty Array), return false", function (assert) {
-    let myArr = [];
-    let deck = myArr;
-    let succeeded = shuffleDeck(deck);
-
-    assert.strictEqual(deck, myArr, "Assert that deck has not been modified.");
-    assert.equal(succeeded, false, "Assert that shuffle failed.");
-});
-
-QUnit.test("shuffleDeck - Modified (int array) deck is an Array.", function (assert) {
-    let deck = [2,3,4,5,6,7,8,9,10,11,12,13,14];
-    const succeeded = shuffleDeck(deck);
+QUnit.test("Cards.shuffle - Shuffled Cards.items (int array) is still an Array.", function (assert) {
+    let cardsObject = new Cards([2,3,4,5,6,7,8,9,10,11,12,13,14]);
+    let succeeded = cardsObject.shuffle();
     
-    assert.strictEqual(deck instanceof Array, true, "Assert modified deck is an Array.");
+    assert.true(cardsObject.items instanceof Array, "Assert modified deck is an Array.");
     assert.equal(succeeded, true, "Assert that shuffle succeeded.");
 });
 
-QUnit.test("shuffleDeck - Modified (string array) deck is an Array.", function (assert) {
-    let deck = ["lorem", "ipsum", "dolor", "sit", "amet"];
-    const succeeded = shuffleDeck(deck);
+QUnit.test("Cards.shuffle - Shuffled Cards.items (String array) is still an Array.", function (assert) {
+    let cardsObject = new Cards(["lorem", "ipsum", "dolor", "sit", "amet"]);
+    let succeeded = cardsObject.shuffle();
     
-    assert.strictEqual(deck instanceof Array, true, "Assert modified deck is an Array.");
+    assert.true(cardsObject.items instanceof Array, "Assert modified deck is an Array.");
     assert.equal(succeeded, true, "Assert that shuffle succeeded.");
 });
 
-QUnit.test("shuffleDeck - Modified (Mixed string/int array) deck is an Array.", function (assert) {
-    let deck = ["lorem", 1, "dolor", 5, "amet"];
-    const succeeded = shuffleDeck(deck);
+QUnit.test("Cards.shuffle - Shuffled Cards.items (Mixed string/int array) deck is still an Array.", function (assert) {
+    let cardsObject = new Cards(["lorem", 1, "dolor", 5, "amet"]);
+    let succeeded = cardsObject.shuffle();
     
-    assert.strictEqual(deck instanceof Array, true, "Assert modified deck is an Array.");
+    assert.true(cardsObject.items instanceof Array, "Assert modified deck is an Array.");
     assert.equal(succeeded, true, "Assert that shuffle succeeded.");
 });
 
-QUnit.test("shuffleDeck - Returned value is random", function (assert) {
+QUnit.test("shuffleDeck - Shuffle is actually random.", function (assert) {
     const originalDeck = [2,3,4,5,6,7,8,9,10,11,12,13,14];
-    let deck = [2,3,4,5,6,7,8,9,10,11,12,13,14];
-    let succeeded = shuffleDeck(deck);
+    let cardsObject = new Cards([2,3,4,5,6,7,8,9,10,11,12,13,14]);
+    let succeeded = cardsObject.shuffle();
 
-    assert.notEqual(deck, originalDeck, "Assert that deck no longer holds its original value.");
+    assert.notEqual(cardsObject.items, originalDeck, "Assert that deck no longer holds its original value.");
     assert.equal(succeeded, true, "Assert that shuffle succeeded.");
 });
 
 QUnit.test(`dealCards - Discards and marks a card as "dead", when odd amount of players with even amount of source deck cards.`, function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let playerDecks = [[], [], []];
+    let originalItems = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
 
     assert.notStrictEqual(deadCard, null, "Dead card is not null.");
-    assert.true(cardDeck.includes(deadCard), "Dead card is one of the ones from the given deck.");
+    assert.true(originalItems.includes(deadCard), "Dead card is one of the ones from the given deck.");
     
     for (let i = 0; i < playerDecks.length; i++) {
-        assert.false(playerDecks[i].includes(deadCard), `Dead card is not in player deck #${i}`);
+        assert.false(playerDecks[i].items.includes(deadCard), `Dead card is not in player deck #${i}`);
     }
 });
 
 QUnit.test("dealCards - Modifies supplied decks.", function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let deck1 = [];
-    let deck2 = []; 
-    let playerDecks = [deck1, deck2];
+    let originalItems = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
 
-    const cardDeckHalfLength = cardDeck.length / 2;
+    const cardDeckHalfLength = originalItems.length / 2;
     for (let i = 0; i < playerDecks.length; i++) {
-        assert.equal(playerDecks[i].length, cardDeckHalfLength, `Modified deck #${i+1}/2 is half (${cardDeckHalfLength}) of cardDeck (${cardDeck.length}).`);
+        assert.equal(playerDecks[i].length, cardDeckHalfLength, `Modified deck #${i+1}/2 is half (${cardDeckHalfLength}) of originalItems (${originalItems.length}).`);
     }
 });
 
 QUnit.test("dealCards - All source card decks have been dealt.", function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let deck1 = [];
-    let deck2 = []; 
-    let playerDecks = [deck1, deck2];
+    let originalItems = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
     
     // For card in source card deck.
-    for (let i = 0; i < cardDeck.length; i++) {
+    for (let i = 0; i < originalItems.length; i++) {
         let cardInDeck = false;
         
         // For deck in playerDecks.
         for (let j = 0; j < playerDecks.length; j++) {
-            if (cardInDeck == false) cardInDeck = playerDecks[j].includes(cardDeck[i]);
+            if (cardInDeck == false) cardInDeck = playerDecks[j].includes(originalItems[i]);
         }
         
-        assert.true(cardInDeck, `Source deck card ${cardDeck[i]} has been dealt to a deck.`);
+        assert.true(cardInDeck, `Source deck card ${originalItems[i]} has been dealt to a deck.`);
     }
 });
 
-QUnit.test("dealCards - Dealt decks consist of cards from the source card deck.", function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let deck1 = [];
-    let deck2 = [];
-    let playerDecks = [deck1, deck2];
+QUnit.test("dealCards - Dealt decks consists of cards from the source card deck.", function (assert) {
+    let originalItems = [1,2,3,4,5,6,7,8,9,10,11,12];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
 
     // For card in source card deck.
-    for (let i = 0; i < cardDeck.length; i++) {
+    for (let i = 0; i < originalItems.length; i++) {
         let cardInDeck = false;
         
         // For deck in playerDecks.
         for (let j = 0; j < playerDecks.length; j++) {
-            if (cardInDeck == false) cardInDeck = playerDecks[j].includes(cardDeck[i]);
+            if (cardInDeck == false) cardInDeck = playerDecks[j].includes(originalItems[i]);
         }
 
-        assert.true(cardInDeck, `Source deck card ${cardDeck[i]} has been dealt to a deck.`);
+        assert.true(cardInDeck, `Source deck card ${originalItems[i]} has been dealt to a deck.`);
     }
     
     // For deck in playerDecks.
     for (let i = 0; i < playerDecks.length; i++) {
         // For card in deck.
         for (let card of playerDecks[i]) {
-            assert.true(cardDeck.includes(card), `Card ${card} from deck #${i+1}/${playerDecks.length} is in source card deck.`);
+            assert.true(originalItems.includes(card), `Card ${card} from deck #${i+1}/${playerDecks.length} is in source card deck.`);
         }
     }
 });
 
 QUnit.test("dealCards - Dealt decks have equal length when even amount.", function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let deck1 = [];
-    let deck2 = [];
-    let playerDecks = [deck1, deck2];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
 
     let referenceLength = playerDecks[0].length;
 
@@ -162,13 +168,10 @@ QUnit.test("dealCards - Dealt decks have equal length when even amount.", functi
 });
 
 QUnit.test("dealCards - Dealt decks have equal length when odd amount.", function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let deck1 = [];
-    let deck2 = [];
-    let deck3 = [];
-    let playerDecks = [deck1, deck2, deck3];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
 
     let referenceLength = playerDecks[0].length;
 
@@ -179,12 +182,10 @@ QUnit.test("dealCards - Dealt decks have equal length when odd amount.", functio
 });
 
 QUnit.test("dealCards - Dealt decks have no card in common.", function (assert) {
-    let cardDeck = [1,2,3,4,5,6,7,8,9,10,11,12];
-    let deck1 = [];
-    let deck2 = [];
-    let playerDecks = [deck1, deck2];
+    let cardsObject = new Cards([1,2,3,4,5,6,7,8,9,10,11,12]);
+    let playerDecks = [new Cards([]), new Cards([])];
 
-    dealCards(playerDecks, cardDeck);
+    dealCards(playerDecks, cardsObject);
 
     // Compare decks with eachother:
     // For each deck.
