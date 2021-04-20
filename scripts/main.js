f7e5065519f40dc4563a8922a629019082376a90
BluABK <bluabk@bluabk.net>
2021-04-20 10:10:39 +0200

    Split utils into separate JS file.

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,6 @@ const SUITS = ["heart", "diamond", "spade", "club"];
 // Card that cannot be used in play (usually happens with odd amount of players with an even amount of source deck cards).
 let deadCard = null;
 
-/**
- * Generate UUID v4 string to use for IDs and similiar.
- * @returns {String} An UUID.
- */
-function uuidv4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-    });
-}
-
 let lastBattleVictor = null;
 let lastBattle = {
     "p1Card": null,
